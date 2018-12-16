0bb05df95a304b23e41ba09e06ee690a1374edce
andreaMT15 <andrea.minhas@gmail.com>
2018-12-16 01:41:42 -0600

    total displaying

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,5 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var productInput = [];
-var idInput = [];
-var qtyInput = [];
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -21,7 +18,6 @@ connection.connect(function(err) {
 function displayItems() {
   connection.query("SELECT * from products;", function(error, results, fields) {
     if (error) throw error;
-    // console.log(results);
     for (var i = 0; i < results.length; i++) {
       console.log(
         "Item id: " +
@@ -59,14 +55,7 @@ function itemSelection() {
         }
       ])
       .then(answers => {
-        console.log(answers);
         checkStock(answers.id, answers.quantity);
-        // idInput.push(answers.id);
-        // productInput.push(answers.product);
-        // qtyInput.push(answers.quantity);
-        // console.log(idInput);
-        // console.log(qtyInput);
-        // console.log(productInput);
       });
   });
 }
@@ -74,14 +63,13 @@ displayItems();
 itemSelection();
 
 function checkStock(id, qty) {
-  // console.log(id);
   var query = "SELECT stock_quantity FROM products WHERE item_id=" + id + ";";
   connection.query(query, function(error, response) {
     if (error) throw error;
-    console.log(response[0].stock_quantity);
+
     if (qty <= response[0].stock_quantity) {
       var newqty = response[0].stock_quantity - qty;
-      console.log(newqty);
+
       connection.query("UPDATE products SET ? WHERE item_id=" + id, [
         {
           stock_quantity: newqty
@@ -90,8 +78,18 @@ function checkStock(id, qty) {
           id: id
         }
       ]);
-      console.log("stock updated");
-      connection.end();
+      calculatePrice(id, qty);
     }
+  });
+}
+
+function calculatePrice(id, qty) {
+  connection.query("SELECT price from products WHERE item_id=" + id, function(
+    error,
+    results
+  ) {
+    if (error) throw error;
+    var total = results[0].price * qty;
+    console.log("Your total is: " + total);
   });
 }
